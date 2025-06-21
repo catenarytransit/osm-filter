540fdd9e088b73b14595e3a475db6b1e2f28d5fb
Kyler Chin <kyler@catenarymaps.org>
2025-06-21 11:54:55 -0700

    Create filter_europa_rail.sh

---
+++ filter_europa_rail.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/catenarytransit/pfaedled-gtfs-actions/refs/heads/main/railway-filter.txt -O railway-filter.txt
+
+wget https://download.geofabrik.de/europe-latest.osm.pbf -O europe-latest.osm.pbf
+
+ osmium tags-filter -o railonly-europe-latest.osm.pbf --expressions=railway-filter.txt europe-latest.osm.pbf
+
+ gh release upload latest railonly-europe-latest.osm.pbf --clobber -R https://github.com/catenarytransit/osm-filter/
