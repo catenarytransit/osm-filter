9154681b0b8db67580d123ced0d95ac6584a3d2c
Kyler Chin <kyler@catenarymaps.org>
2026-01-10 22:15:12 -0800

    filter american rail stations

--- filter-usa-rail.sh
+++ filter-usa-rail.sh
@@ -1,7 +1,9 @@
-wget https://raw.githubusercontent.com/catenarytransit/pfaedled-gtfs-actions/refs/heads/main/railway-filter.txt -O railway-filter.txt
-
 wget https://download.geofabrik.de/north-america/us-latest.osm.pbf -O us-latest.osm.pbf
 
- osmium tags-filter -o railonly-us-latest.osm.pbf --expressions=railway-filter.txt us-latest.osm.pbf
+ osmium tags-filter -o railonly-us-latest.osm.pbf --expressions=railway-filter.txt us-latest.osm.pbf --overwrite
 
  gh release upload latest railonly-us-latest.osm.pbf --clobber -R https://github.com/catenarytransit/osm-filter/
+
+ osmium tags-filter -o railstations-us-latest.osm.pbf --expressions=railway-station-filter.txt us-latest.osm.pbf --overwrite
+
+ gh release upload latest railstations-us-latest.osm.pbf --clobber -R https://github.com/catenarytransit/osm-filter/
